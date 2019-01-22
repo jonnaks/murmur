345e97886d31b24eea7e7e652a0e904564bdef87
Jonna Karlsson Sellén <jonna.karlsson.sellen@prototyp.se>
2019-01-22 11:40:57 +0100

    Add camera component

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import CameraEx from './components/camera';
+
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <CameraEx />
       </View>
     );
   }
